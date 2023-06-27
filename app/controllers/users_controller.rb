93f06de56e72bd8f733a8c736807f8285f2342ae
Aliyu <biggy0023@gmail.com>
2023-06-27 14:30:25 +0100

    added edit.html.erb, upgraded the erb to use form_with and added an error message generator. With this, I believe this project section is completed.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,25 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-    # @user = User.new(user_params)
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
     if @user.save
       redirect_to users_path
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to users_path
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
